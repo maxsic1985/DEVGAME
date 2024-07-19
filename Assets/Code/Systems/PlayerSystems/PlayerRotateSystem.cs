8083e45d5399afe015c89e23485d177a84aee63f
Maksim S <maxsic1985@gmail.com>
2024-07-19 08:04:25 +0300

    rotate

--- Assets/Code/Systems/PlayerSystems/PlayerRotateSystem.cs
+++ Assets/Code/Systems/PlayerSystems/PlayerRotateSystem.cs
@@ -9,6 +9,7 @@ namespace MSuhininTestovoe.Devgame
     public class PlayerRotateSystem : IEcsInitSystem, IEcsRunSystem
     {
         private EcsFilter _playerFilter;
+        private EcsFilter _cameraFilter;
         private EcsPool<PlayerInputComponent> _playerInputComponentPool;
         private EcsPool<TransformComponent> _transformComponentPool;
         private ITimeService _timeService;
@@ -22,10 +23,12 @@ namespace MSuhininTestovoe.Devgame
             _sharedData = systems.GetShared<SharedData>().GetPlayerSharedData;
             _playerFilter = world
                 .Filter<IsPlayerComponent>()
-                .Inc<IsPlayerCanAttackComponent>()
+               // .Inc<IsPlayerCanAttackComponent>()
                 .Inc<PlayerInputComponent>()
                 .Inc<TransformComponent>()
                 .End();
+            _cameraFilter = world.Filter<IsCameraComponent>().End();
+            
             _playerInputComponentPool = world.GetPool<PlayerInputComponent>();
             _transformComponentPool = world.GetPool<TransformComponent>();
             _timeService = Service<ITimeService>.Get();
@@ -41,8 +44,9 @@ namespace MSuhininTestovoe.Devgame
 
                 if (playerInputComponent.Rotate)
                 {
+                    Vector3 mousePosition =GameObject.Find("Camera(Clone)").GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
                     var position = transformComponent.Value;
-                    Vector2 direction = Input.mousePosition - position.position;
+                    Vector2 direction = mousePosition - position.position;
                     float angle = Vector2.SignedAngle(Vector2.right, direction);
                     Vector3 targetRotation = new Vector3(0, 0, angle);
                     position.rotation = Quaternion.RotateTowards(position.rotation, Quaternion.Euler(targetRotation),  _sharedData.GetPlayerCharacteristic.RotateSpeed * _timeService.DeltaTime);
