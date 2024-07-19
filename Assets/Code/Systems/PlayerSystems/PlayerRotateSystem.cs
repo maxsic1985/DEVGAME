69a58b81592df21e228bb1feda7f1bcaf23ca715
Maksim S <maxsic1985@gmail.com>
2024-07-19 08:19:49 +0300

    Update PlayerRotateSystem.cs

--- Assets/Code/Systems/PlayerSystems/PlayerRotateSystem.cs
+++ Assets/Code/Systems/PlayerSystems/PlayerRotateSystem.cs
@@ -23,7 +23,6 @@ namespace MSuhininTestovoe.Devgame
             _sharedData = systems.GetShared<SharedData>().GetPlayerSharedData;
             _playerFilter = world
                 .Filter<IsPlayerComponent>()
-               // .Inc<IsPlayerCanAttackComponent>()
                 .Inc<PlayerInputComponent>()
                 .Inc<TransformComponent>()
                 .End();
@@ -49,29 +48,12 @@ namespace MSuhininTestovoe.Devgame
                     Vector2 direction = mousePosition - position.position;
                     float angle = Vector2.SignedAngle(Vector2.right, direction);
                     Vector3 targetRotation = new Vector3(0, 0, angle);
-                    position.rotation = Quaternion.RotateTowards(position.rotation, Quaternion.Euler(targetRotation),  _sharedData.GetPlayerCharacteristic.RotateSpeed * _timeService.DeltaTime);
-                    Debug.Log("Rot");
-                    
-                    
-//                    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                  //  Vector3 direction = Vector3.up * inputComponent.Vertical + Vector3.right * inputComponent.Horizontal;
-                //   var  A= Mathf.LerpAngle(transformComponent.Value.position.x, Input.mousePosition.x, 1);
-                  //    transformComponent.Value.position = Vector2.MoveTowards(transformComponent.Value.position, Input.mousePosition,  _sharedData.GetPlayerCharacteristic.Speed * _timeService.DeltaTime);
-                  // Vector3.Lerp( transformComponent.Value.position,
-                  //     transformComponent.Value.position+direction,
-                  //     _sharedData.GetPlayerCharacteristic.Speed * _timeService.DeltaTime);
+                    position.rotation = Quaternion.RotateTowards(
+                        position.rotation,
+                        Quaternion.Euler(targetRotation),
+                        _sharedData.GetPlayerCharacteristic.RotateSpeed * _timeService.DeltaTime);
                 }
             }
         }
-
-      
-        // private void PlayerMoving(ref TransformComponent transformComponent, ref PlayerInputComponent inputComponent)//,ref DestinationComponent destinationComponent)
-        // {
-        //     Vector3 direction = Vector3.up * inputComponent.Vertical + Vector3.right * inputComponent.Horizontal;
-        //    
-        //  transformComponent.Value.position = Vector3.Lerp( transformComponent.Value.position,
-        //      transformComponent.Value.position+direction,
-        //      _sharedData.GetPlayerCharacteristic.Speed * _timeService.DeltaTime);
-        // }
     }
 }
